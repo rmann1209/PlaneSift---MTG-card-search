0af60fd67f3f0018399bbe47d9a6f3ef09f01364
Robert Mann <91228383+rmann1209@users.noreply.github.com>
2022-11-20 17:37:13 -0500

    Add files via upload
    Added Card class to main

--- main.py
+++ main.py
@@ -1,59 +1,20 @@
-from mimetypes import init
-import csv
+class Card:
+    def __init__(self, name, super, type, sub, power, tough, color, cmc, text, rarity, url):
+        self.name = name;
+        self.super = super;
+        self.type = type;
+        self.sub = sub;
+        self.power = power;
+        self.tough = tough;
+        self.color = color;
+        self.cmc = cmc;
+        self.text = text;
+        self.rarity = rarity;
+        self.url = url;
 
-file = open("cards(pipe).txt", "r");
-for line in file:
-    cards = line.split("|")
-    name = cards[0].toLowerCase()
-    cmc = cards[1]
-    color = cards[2]
-    typ = cards[3]
-    supTyp = cards[4]
-    subTyp = cards[5].toLowerCase()
-    rarity = cards[6]
-    text = cards[7].toLowerCase()
-    power = cards[8]
-    tough = cards[9]
-    url = cards[10]
+    def print(self):
+        print(self.name + " " + str(self.power));
 
-    if (url == ""):
-        continue
 
-    temp = ""
-
-    if (typ.__contains__("Artifact")):
-        temp += "artifact "
-
-    if (typ.__contains__("Creature")):
-        temp += "creature "
-
-    if (typ.__contains__("Enchantment")) :
-        temp += "enchantment "
-
-    if (typ.__contains__("Instant")) :
-        temp += "instant "
-
-    if (typ.__contains__("Land")) :
-        temp += "land "
-
-    if (typ.__contains__("Sorcery")) :
-        temp += "sorcery "
-
-    typ = temp
-    temp = ""
-
-    if(supTyp.contains("Legendary")):
-        temp += "legendary "
-
-    if(supTyp.contains("Basic")):
-        temp += "basic "
-
-    if(supTyp.contains("Snow")):
-        temp += "snow "
-
-    if(supTyp.contains("World")):
-        temp += "world "
-
-    subTyp = temp
-
-    card = Card(name, cmc, color, typ, supTyp, subTyp, rarity, text, power, tough, url)
+testCard = Card("name", "super", "type", "sub", 3, 4, "blue", 7, "asdhf uh", "f jajsdifj ", "idk.com");
+testCard.print();
